d571cd5a5d8b677d3a951b96e5dc8a79bd61e16e
Kaushik <181184942+kaushik-dwarakanath@users.noreply.github.com>
2026-04-19 17:19:29 +0530

    completed object_write with atomic temp file write, fsync and rename

--- object.c
+++ object.c
@@ -132,12 +132,25 @@ int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out
     snprintf(shard_dir, sizeof(shard_dir), "%s/%.2s", OBJECTS_DIR, hex);
     mkdir(shard_dir, 0755);
 
+    // write temp file, fsync, rename
+    char final_path[512];
+    object_path(id_out, final_path, sizeof(final_path));
+    char tmp_path[520];
+    snprintf(tmp_path, sizeof(tmp_path), "%s.tmp", final_path);
 
+    int fd = open(tmp_path, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+    if (fd < 0) { free(obj); return -1; }
+    if (write(fd, obj, total) != (ssize_t)total) { close(fd); free(obj); return -1; }
+    fsync(fd);
+    close(fd);
     free(obj);
 
-    
+    if (rename(tmp_path, final_path) != 0) return -1;
 
-    return -1;
+    int dir_fd = open(shard_dir, O_RDONLY);
+    if (dir_fd >= 0) { fsync(dir_fd); close(dir_fd); }
+
+    return 0;
 }
 
 // Read an object from the store.
